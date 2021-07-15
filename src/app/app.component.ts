70bd8ad7c42bc3f8230663bb3af4da330040850c
neha9461 <nehagoyalneha9461@gmail.com>
2021-07-15 16:46:59 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component, VERSION } from '@angular/core';
 
+export declare interface Candidate {
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  fName = '';
+  lName = '';
+  email = '';
+  gender = 'Male';
+  candidateList: Candidate[] = [];
+  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'gender'];
+
+  addCandidate() {
+    const candidate: Candidate = {
+      firstName: this.fName,
+      lastName: this.lName,
+      email: this.email,
+      gender: this.gender === '1' ? 'Male' : 'Female'
+    };
+    this.candidateList.push(candidate);
+    console.log(this.candidateList);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatRadioModule,
+    MatInputModule,
+    MatTableModule
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
